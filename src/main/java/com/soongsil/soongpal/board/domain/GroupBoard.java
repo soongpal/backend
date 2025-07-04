650e3df9428d7c6a68b9a52bd19164ee91c49cf2
Huiyeongkim <huiyeong9619@naver.com>
2025-07-04 12:18:04 +0900

    feat: 공동구매 엔티티 생성

--- src/main/java/com/soongsil/soongpal/board/domain/GroupBoard.java
+++ src/main/java/com/soongsil/soongpal/board/domain/GroupBoard.java
@@ -1,14 +1,22 @@
 package com.soongsil.soongpal.board.domain;
 
 import com.soongsil.soongpal.common.domain.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+
 public class GroupBoard extends BaseEntity {
 
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String title;
 
+    @Lob
+    private String content;
+
+    private String url;
+
+    private String location;
 }
