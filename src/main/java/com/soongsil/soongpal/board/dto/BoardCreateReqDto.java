88feccc0f7f285edb9d707d6473396855f3cfb1f
Yeun1104 <bliass9050@gmail.com>
2025-07-12 23:06:10 +0900

    feat: status 필드와 값 추가

--- src/main/java/com/soongsil/soongpal/board/dto/BoardCreateReqDto.java
+++ src/main/java/com/soongsil/soongpal/board/dto/BoardCreateReqDto.java
@@ -1,7 +1,9 @@
 package com.soongsil.soongpal.board.dto;
 
 import com.soongsil.soongpal.board.domain.Board;
+import com.soongsil.soongpal.board.domain.BoardStatus;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 
@@ -17,12 +19,16 @@ public class BoardCreateReqDto {
     private String url;
     private String location;
 
+    @NotNull
+    private BoardStatus status;
+
     public static Board toEntity(BoardCreateReqDto boardCreateReqDto) {
         return Board.builder()
                 .title(boardCreateReqDto.getTitle())
                 .content(boardCreateReqDto.getContent())
                 .url(boardCreateReqDto.getUrl())
                 .location(boardCreateReqDto.getLocation())
+                .status(boardCreateReqDto.getStatus())
                 .build();
     }
 }
