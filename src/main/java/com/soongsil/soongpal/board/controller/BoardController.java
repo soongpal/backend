14dc659cdaef5d2220ceea09962b455cbadad395
Yeun1104 <bliass9050@gmail.com>
2025-07-12 22:46:39 +0900

    refactor: GroupBoardController의 코드 복제 및 group -> board로 이름 수정

--- src/main/java/com/soongsil/soongpal/board/controller/BoardController.java
+++ src/main/java/com/soongsil/soongpal/board/controller/BoardController.java
@@ -1,8 +1,51 @@
 package com.soongsil.soongpal.board.controller;
 
-import org.springframework.stereotype.Controller;
+import com.soongsil.soongpal.board.dto.BoardCreateReqDto;
+import com.soongsil.soongpal.board.dto.BoardResDto;
+import com.soongsil.soongpal.common.dto.CommonResDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@RequestMapping("/api/board-purchases")
+@RestController
 public class BoardController {
 
+    private final BoardService BoardService;
+
+    @PostMapping
+    public ResponseEntity<CommonResDto<BoardResDto>> createBoard(@Valid @RequestBody BoardCreateReqDto BoardCreateReqDto) {
+        BoardResDto dto = BoardService.createBoard(BoardCreateReqDto);
+        return new ResponseEntity<>(new CommonResDto<>("공동구매 게시글 생성", dto), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<CommonResDto<List<BoardResDto>>> getAllBoards() {
+        List<BoardResDto> dto = BoardService.getAllBoards();
+        return new ResponseEntity<>(new CommonResDto<>("모든 공동구매 게시글 조회", dto), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CommonResDto<BoardResDto>> getBoardById(@PathVariable Long id) {
+        BoardResDto dto = BoardService.getBoardById(id);
+        return new ResponseEntity<>(new CommonResDto<>("공동구매 게시글 상세 조회", dto), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CommonResDto<BoardResDto>> updateBoard(@PathVariable Long id, @Valid @RequestBody BoardCreateReqDto BoardCreateReqDto) {
+        BoardResDto dto = BoardService.updateBoard(id, BoardCreateReqDto);
+        return new ResponseEntity<>(new CommonResDto<>("공동구매 게시글 수정", dto), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<CommonResDto<BoardResDto>> deleteBoard(@PathVariable Long id) {
+        BoardResDto dto = BoardService.deleteBoard(id);
+        return new ResponseEntity<>(new CommonResDto<>("공동구매 게시글 삭제", dto), HttpStatus.OK);
+    }
 }
