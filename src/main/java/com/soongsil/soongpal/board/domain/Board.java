7a4b0cc16fb800021b221993b01dc365b96031cf
Yeun1104 <bliass9050@gmail.com>
2025-07-14 23:02:24 +0900

    feat: status 값 업데이트 코드 추가

--- src/main/java/com/soongsil/soongpal/board/domain/Board.java
+++ src/main/java/com/soongsil/soongpal/board/domain/Board.java
@@ -34,5 +34,6 @@ public class Board extends BaseEntity {
         this.content = board.getContent();
         this.url = board.getUrl();
         this.location = board.getLocation();
+        this.status = board.getStatus();
     }
 }
