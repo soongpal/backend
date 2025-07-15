879efcd5f90f631449b265745c3755702ca57ce9
Yeun1104 <bliass9050@gmail.com>
2025-07-15 17:40:26 +0900

    feat: 게시글 swagger 적용

---
+++ src/main/java/com/soongsil/soongpal/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.soongsil.soongpal.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("SoongPal API 문서")
+                .version("v1.0.0")
+                .description("숭실대학교 학생들을 위한 공동구매와 중고거래 플랫폼 입니다. 이 API 문서는 SoongPal의 API 사용법을 설명합니다.");
+
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
+    }
+
+}
