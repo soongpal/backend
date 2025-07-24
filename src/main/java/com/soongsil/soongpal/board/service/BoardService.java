674be984b0b07648b77b95752fddd1613c490d18
Yeun1104 <bliass9050@gmail.com>
2025-07-25 00:34:45 +0900

    refector: 사용하지 않는 import 삭제

--- src/main/java/com/soongsil/soongpal/board/service/BoardService.java
+++ src/main/java/com/soongsil/soongpal/board/service/BoardService.java
@@ -19,9 +19,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import org.springframework.data.domain.Pageable;
-import java.util.stream.Collectors;
-
-import java.util.List;
 
 
 @Slf4j
