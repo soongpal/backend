0bf702e50579ce6d05c29eff03364275b79455d1
Huiyeongkim <huiyeong9619@naver.com>
2025-07-25 14:48:13 +0900

    feat: 채팅 config

---
+++ src/main/java/com/soongsil/soongpal/chat/controller/ChatController.java
@@ -0,0 +1,10 @@
+package com.soongsil.soongpal.chat.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+
+
+
+}
---
+++ src/main/java/com/soongsil/soongpal/chat/dto/ChatMessageDto.java
@@ -0,0 +1,9 @@
+package com.soongsil.soongpal.chat.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ChatMessageDto {
+}
