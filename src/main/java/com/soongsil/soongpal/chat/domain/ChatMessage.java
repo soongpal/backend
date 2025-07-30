f7ab47301f7aaef7758e908761bad11114c8de2e
Huiyeongkim <huiyeong9619@naver.com>
2025-07-31 04:28:11 +0900

    feat: 채팅 메시지 엔티티 & 레포지토리 생성

---
+++ src/main/java/com/soongsil/soongpal/chat/domain/ChatMessage.java
@@ -0,0 +1,29 @@
+package com.soongsil.soongpal.chat.domain;
+
+
+import com.soongsil.soongpal.common.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ChatMessage extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id")
+    private ChatRoom chatRoom;
+
+    private Long senderId;
+    private String message;
+
+}
