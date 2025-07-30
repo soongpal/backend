bcf797b93fa91180086aac109a1c513896fe556f
Huiyeongkim <huiyeong9619@naver.com>
2025-07-31 04:00:44 +0900

    feat: 유저 도메인 생성

---
+++ src/main/java/com/soongsil/soongpal/user/domain/User.java
@@ -0,0 +1,21 @@
+package com.soongsil.soongpal.user.domain;
+
+import com.soongsil.soongpal.common.domain.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nickName;
+
+}
