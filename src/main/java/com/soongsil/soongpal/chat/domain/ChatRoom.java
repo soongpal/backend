7c6e898569c030b0eb259aae20727550221f33ad
Huiyeongkim <huiyeong9619@naver.com>
2025-07-31 04:21:19 +0900

    feat: 채팅 엔티티 구현

---
+++ src/main/java/com/soongsil/soongpal/chat/domain/ChatRoom.java
@@ -0,0 +1,29 @@
+package com.soongsil.soongpal.chat.domain;
+
+import com.soongsil.soongpal.common.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "chat_rooms")
+public class ChatRoom extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL)
+    private List<ChatRoomUser> chatRoomUsers = new ArrayList<>();
+}
