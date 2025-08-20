7cf9883e7f8033a76c633de753b4631962ad082b
Yeun1104 <bliass9050@gmail.com>
2025-08-20 23:11:52 +0900

    feat: 게시글 조회 API 비로그인 접근 허용

--- src/main/java/com/soongsil/soongpal/common/config/SecurityConfig.java
+++ src/main/java/com/soongsil/soongpal/common/config/SecurityConfig.java
@@ -7,19 +7,15 @@ import com.soongsil.soongpal.user.service.CustomOAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer; // csrf disable 방식 변경
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -45,6 +41,7 @@ public class SecurityConfig {
                             "/login/oauth2/code/**",
                             "/api/auth/**"
                     ).permitAll()
+                    .requestMatchers(HttpMethod.GET, "/api/board/**").permitAll()
                     .anyRequest().authenticated()
             )
             .oauth2Login(oauth2 -> oauth2
