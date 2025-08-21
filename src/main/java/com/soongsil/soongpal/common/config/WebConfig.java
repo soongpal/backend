26760f30bfab365b1d8ffd7779752c5ed9c9aebd
Yeun1104 <bliass9050@gmail.com>
2025-08-21 15:30:20 +0900

    fix: 주소 수정

--- src/main/java/com/soongsil/soongpal/common/config/WebConfig.java
+++ src/main/java/com/soongsil/soongpal/common/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://soongpal.shop", "http://localhost:5173")
+                .allowedOrigins("https://soongpal.shop", "http://localhost:5173/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
