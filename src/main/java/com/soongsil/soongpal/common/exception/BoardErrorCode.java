acf8b76080e338760981a162c0a69745ddbad8f1
Huiyeongkim <huiyeong9619@naver.com>
2025-09-16 17:55:18 +0900

    refactor: board service 커스텀 에러로 변경

--- src/main/java/com/soongsil/soongpal/common/exception/BoardErrorCode.java
+++ src/main/java/com/soongsil/soongpal/common/exception/BoardErrorCode.java
@@ -13,7 +13,8 @@ public enum BoardErrorCode {
     BOARD_DELETE_DENIED(HttpStatus.FORBIDDEN, "게시글을 삭제할 권한이 없습니다."),
     INVALID_BOARD_DATA(HttpStatus.BAD_REQUEST, "유효하지 않은 게시글 데이터입니다."),
     BOARD_ALREADY_DELETED(HttpStatus.CONFLICT, "이미 삭제된 게시글입니다."),
-    BOARD_FILE_UPLOAD_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "게시글 파일 업로드 중 오류가 발생했습니다.");
+    BOARD_FILE_UPLOAD_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "게시글 파일 업로드 개수를 초과했습니다."),
+    LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "좋아요를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
