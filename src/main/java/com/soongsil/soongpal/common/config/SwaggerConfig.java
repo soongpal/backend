a04ca063e6af89cf909f79c03bb40bfa2c0e7b3b
Huiyeongkim <huiyeong9619@naver.com>
2025-09-18 16:54:25 +0900

    feat: 스웨거 jwt 추가

--- src/main/java/com/soongsil/soongpal/common/config/SwaggerConfig.java
+++ src/main/java/com/soongsil/soongpal/common/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.soongsil.soongpal.common.config;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,8 +18,19 @@ public class SwaggerConfig {
                 .version("v1.0.0")
                 .description("숭실대학교 학생들을 위한 공동구매와 중고거래 플랫폼 입니다. 이 API 문서는 SoongPal의 API 사용법을 설명합니다.");
 
+        SecurityScheme jwtScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("JWT");
+
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components().addSecuritySchemes("JWT", jwtScheme))
+                .addSecurityItem(securityRequirement)
                 .info(info);
     }
 
